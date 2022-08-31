1c06c422d81fe09bbaa556812ae0f34d9f1e5487
Jared Bruni <jaredbruni@gmail.com>
2022-08-31 10:44:51 -0700

    added mouse

--- puzzle.js
+++ puzzle.js
@@ -49,10 +49,118 @@ function main(puzzle) {
     loadGraphics();
     grid.clearGrid();
     window.addEventListener("keydown", keyPressed, true);
+
+    var istouch = 'ontouchstart' in window;
+
+    if(istouch) {
+        /*
+	    window.addEventListener("touchstart",mouseDown,true);
+    	window.addEventListener("touchend",mouseUp,true);
+        window.addEventListener("touchmove", mouseMove, true);
+        */
+    }
+    else {
+	    window.addEventListener("mouseup", mouseUp, true);
+	    window.addEventListener("mousedown", mouseDown, true);
+        window.addEventListener("mousemove", mouseMoveB, true); 
+        window.addEventListener("dblclick", doubleClick, true);
+    }
+
+    
+
     setInterval(drawScreen, 25);
     drawScreen();
 }
 
+var downX = 0, downY = 0,upX = 0, upY = 0;
+var old_down_X = 0;
+
+
+var old_pos = 0;
+var is_down = false;
+var is_moved = false;
+
+function doubleClick() {
+    grid.keyRotateLeft();
+}
+
+function mouseMove(e) {
+	var sourceElement = e.target || e.srcElement;	
+	e.preventDefault();
+	upX = e.touches[0].pageX;
+	upY = e.touches[0].pageY;
+    if(is_down === true) {
+        var xpos = e.touches[0].pageX;
+        var ypos = e.touches[0].pageY;
+
+        if(xpos > downX+50) {
+            downX = xpos;
+            grid.keyRight();
+            is_moved = true;
+        } else if (xpos < downX-50) {
+            downX = xpos;
+            grid.keyLeft();
+            is_moved = true;
+        }
+
+    }
+    e.preventDefault();
+}
+
+
+function mouseMoveB(e) {
+    if(is_down === true) {
+        var xpos = e.pageX;
+        var ypos = e.pageY;
+        if(xpos > downX+50) {
+            downX = xpos;
+            grid.keyRight();
+            is_moved = true;
+        } else if (xpos < downX-50) {
+            downX = xpos;
+            grid.keyLeft();
+            is_moved = true;
+        }
+    }
+    e.preventDefault();
+}
+
+function mouseDown(e) {
+		downX = e.pageX;
+        downY = e.pageY;
+        is_down = true;
+}
+
+function mouseUp(e) {
+    
+    is_down = false;
+    
+	if(downX === 0 && downY === 0) return;
+	if(e.type === "touchend") {
+		upX = e.changedTouches[0].pageX;
+		upY = e.changedTouches[0].pageY;	
+	} else {
+		upX = e.pageX;
+		upY = e.pageY;
+	}
+	var deltaX = downX - upX;
+	var deltaY = downY - upY;
+	if(Math.abs(deltaX) > 100) {
+		if(is_moved === false && deltaX < 0) { 
+			grid.keyRight();
+		}
+		if(is_moved === false && deltaX > 0) { 
+			grid.keyLeft(); 
+		}	
+	} else if(Math.abs(deltaY) > 50) {
+		if(deltaY > 0) { grid.keyShiftUp(); }
+		if(deltaY < 0) { grid.keyDown(); }
+	}
+
+    if(is_moved === true) {
+        is_moved = false;
+    }
+} 
 function keyPressed(key) {
 
     if(grid.gameOver() === false) {
