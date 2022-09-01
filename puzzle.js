3aafb5669a1246b9e07f7ece58b7269379ac1cee
Jared Bruni <jaredbruni@gmail.com>
2022-08-31 20:18:43 -0700

    formatting

--- puzzle.js
+++ puzzle.js
@@ -3,15 +3,15 @@ var grid;
 const GRID_WIDTH=1280/64;
 const GRID_HEIGHT=720/32;
 const BLOCK_NULL = 0;
-const  BLOCK_CLEAR = 1;
+const BLOCK_CLEAR = 1;
 const BLOCK_MATCH = 11;
 
 var Module = {
-      onRuntimeInitialized: function() {
-        grid = new Module.PuzzleGrid(GRID_WIDTH, GRID_HEIGHT);
-        grid.init_rand(Math.floor(Math.random()*1000));
-        main(grid);
-      }
+onRuntimeInitialized: function() {
+    grid = new Module.PuzzleGrid(GRID_WIDTH, GRID_HEIGHT);
+    grid.init_rand(Math.floor(Math.random()*1000));
+    main(grid);
+}
 };
 
 var can = document.getElementById("can1");
@@ -22,13 +22,13 @@ var level = []
 function loadGraphics() {
     var graphics = ['red1.png', 'red2.png', 'red3.png', 'green1.png', 'green2.png', 'green3.png', 'blue1.png', 'blue2.png', 'blue3.png'];
     var level_gfx = ['level1', 'level2', 'level3', 'level4', 'level5', 'level6', 'level7' ];
-
+    
     for(var i = 0; i < graphics.length; ++i) {
-		images[i+2] = new Image();
-		images[i+2].onload = function() {}
-		images[i+2].src = "img/" + graphics[i];	
-	}
-
+        images[i+2] = new Image();
+        images[i+2].onload = function() {}
+        images[i+2].src = "img/" + graphics[i];
+    }
+    
     for(i = 0; i <= 6; ++i) {
         level[i] = new Image();
         level[i].onload = function() {}
@@ -41,17 +41,17 @@ function main(puzzle) {
     grid.clearGrid();
     window.addEventListener("keydown", keyPressed, true);
     var istouch = 'ontouchstart' in window;
-    if(istouch) {    
-	    window.addEventListener("touchstart",mouseDown, true);
-    	window.addEventListener("touchend",mouseUp, true);
+    if(istouch) {
+        window.addEventListener("touchstart",mouseDown, true);
+        window.addEventListener("touchend",mouseUp, true);
         window.addEventListener("touchmove", mouseMove, true);
     }
     else {
-	    window.addEventListener("mouseup", mouseUp, true);
-	    window.addEventListener("mousedown", mouseDown, true);
-        window.addEventListener("mousemove", mouseMoveB, true); 
+        window.addEventListener("mouseup", mouseUp, true);
+        window.addEventListener("mousedown", mouseDown, true);
+        window.addEventListener("mousemove", mouseMoveB, true);
     }
-
+    
     setInterval(drawScreen, 25);
     drawScreen();
 }
@@ -65,13 +65,13 @@ var is_down = false;
 var is_moved = false;
 
 function mouseMove(e) {
-	var sourceElement = e.target || e.srcElement;	
-	upX = e.touches[0].pageX;
-	upY = e.touches[0].pageY;
+    var sourceElement = e.target || e.srcElement;
+    upX = e.touches[0].pageX;
+    upY = e.touches[0].pageY;
     if(is_down === true) {
         var xpos = e.touches[0].pageX;
         var ypos = e.touches[0].pageY;
-
+        
         if(xpos > downX+50) {
             downX = xpos;
             grid.keyRight();
@@ -81,7 +81,7 @@ function mouseMove(e) {
             grid.keyLeft();
             is_moved = true;
         }
-
+        
     }
     e.stopPropagation();
     e.preventDefault();
@@ -106,15 +106,15 @@ function mouseMoveB(e) {
 }
 
 function mouseDown(e) {
-
+    
     if(e.type === "touchstart")  {
-            downX = e.touches[0].pageX;
-            downY = e.touches[0].pageY;
-
-            if(e.touches.length > 1) {
-                grid.keyRotateLeft();
-            }
-
+        downX = e.touches[0].pageX;
+        downY = e.touches[0].pageY;
+        
+        if(e.touches.length > 1) {
+            grid.keyRotateLeft();
+        }
+        
     } else {
         downX = e.pageX;
         downY = e.pageY;
@@ -126,58 +126,58 @@ function mouseUp(e) {
     
     is_down = false;
     
-	if(downX === 0 && downY === 0) return;
-	if(e.type === "touchend") {
-		upX = e.changedTouches[0].pageX;
-		upY = e.changedTouches[0].pageY;	
-	} else {
-		upX = e.pageX;
-		upY = e.pageY;
-	}
-	var deltaX = downX - upX;
-	var deltaY = downY - upY;
-	if(Math.abs(deltaX) > 100) {
-		if(is_moved === false && deltaX < 0) { 
-			grid.keyRight();
-		}
-		if(is_moved === false && deltaX > 0) { 
-			grid.keyLeft(); 
-		}	
-	} else if(Math.abs(deltaY) > 50) {
-		if(deltaY > 0) { grid.keyShiftUp(); }
-		if(deltaY < 0) { grid.keyDown(); }
-	}
-
+    if(downX === 0 && downY === 0) return;
+    if(e.type === "touchend") {
+        upX = e.changedTouches[0].pageX;
+        upY = e.changedTouches[0].pageY;
+    } else {
+        upX = e.pageX;
+        upY = e.pageY;
+    }
+    var deltaX = downX - upX;
+    var deltaY = downY - upY;
+    if(Math.abs(deltaX) > 100) {
+        if(is_moved === false && deltaX < 0) {
+            grid.keyRight();
+        }
+        if(is_moved === false && deltaX > 0) {
+            grid.keyLeft();
+        }
+    } else if(Math.abs(deltaY) > 50) {
+        if(deltaY > 0) { grid.keyShiftUp(); }
+        if(deltaY < 0) { grid.keyDown(); }
+    }
+    
     if(is_moved === true) {
         is_moved = false;
     }
     e.preventDefault();
 } 
 function keyPressed(key) {
-
+    
     if(grid.gameOver() === false) {
         switch(key.keyCode) {
-		    case 37:
-		        grid.keyLeft();
-		        key.preventDefault();
-		    break;
-		    case 38:
+            case 37:
+                grid.keyLeft();
+                key.preventDefault();
+                break;
+            case 38:
                 grid.keyShiftUp();
-		        key.preventDefault();
-		    break;
+                key.preventDefault();
+                break;
             case 32:
                 grid.keyRotateLeft();
                 key.preventDefault();
                 break;
-		    case 39:
-	    	    grid.keyRight();
-    		    key.preventDefault();
-		    break;
-		    case 40:
-		        grid.keyDown();
-		        key.preventDefault();
-		    break;
-	    }
+            case 39:
+                grid.keyRight();
+                key.preventDefault();
+                break;
+            case 40:
+                grid.keyDown();
+                key.preventDefault();
+                break;
+        }
     } else {
         if(key.keyCode === 13) {
             grid.clearGrid();
@@ -191,15 +191,15 @@ function newGame() {
 }
 
 function drawBlock(num) {
-
+    
     var block_t = grid.block_pos(num);
     var block_x = grid.block_getx(num);
     var block_y = grid.block_gety(num);
-
+    
     if(block_t === BLOCK_MATCH) {
         var r = Math.floor(Math.random()*8);
         context.drawImage(images[2+r], block_x*64, block_y*32);
-    } else { 
+    } else {
         context.drawImage(images[block_t], block_x*64, block_y*32);
     }
 }
@@ -213,9 +213,9 @@ var counter = 0;
 var clear_colors = ['#FF0000', '#00FF00', '#0000FF' ];
 
 function drawScreen() {
-
+    
     logic();
-
+    
     ++counter;
     if(grid.gameOver() === false && counter > 25) {
         counter = 0;
@@ -240,7 +240,7 @@ function drawScreen() {
                         break;
                     case BLOCK_MATCH:
                         var r = Math.floor(Math.random()*8);
-                         context.drawImage(images[2+r], x*64, y*32, 62, 30);
+                        context.drawImage(images[2+r], x*64, y*32, 62, 30);
                         break;
                     default:
                         context.drawImage(images[pos], x*64,y*32, 62, 30);
@@ -249,21 +249,21 @@ function drawScreen() {
                 
             }
         }
-
+        
         drawBlock(0);
         drawBlock(1);
         drawBlock(2);
     } else {
-
+        
         context.fillStyle = "#000000";
         context.fillRect( 50, 50, 1280-100, 720-100 );
         context.fillStyle="#FFFFFF";
         context.font="54px Verdana";
         context.fillText("Game Over Press Return to Start Over", 100, 150);
-
+        
     }
     context.fillStyle="#FFFFFF";
     context.font="24px Verdana";
     context.fillText("Score: " + grid.currentLines() + " Level: " + grid.level(), 25, 25);
-
+    
 }
